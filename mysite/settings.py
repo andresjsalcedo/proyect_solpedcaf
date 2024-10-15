86157eff2dfa661b5ddae44c206b056b34748449
Andrés Salcedo <134748294+andresjsalcedo@users.noreply.github.com>
2024-10-14 22:30:41 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0jb3v_ishkl=t)noy55jg^fy9m*e**w$23866zqx!*ef316gni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.117.180.187', 'localhost']
+ALLOWED_HOSTS = ['18.117.180.187']
 
 
 # Application definition
