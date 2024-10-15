e5e7e6411bb6029a34518c663ea2479098ec21dc
Andrés Salcedo <134748294+andresjsalcedo@users.noreply.github.com>
2024-10-14 23:18:08 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     "default":{
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'solpedcaf',
+        'NAME': 'proyect_solpedcaf',
         'USER': 'root',
         'PASSWORD': "root",
         'HOST': 'localhost',
