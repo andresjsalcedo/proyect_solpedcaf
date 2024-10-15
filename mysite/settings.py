d2331fedf6152df192ed70aa05edae1a37840502
Andrés Salcedo <134748294+andresjsalcedo@users.noreply.github.com>
2024-10-14 22:26:20 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0jb3v_ishkl=t)noy55jg^fy9m*e**w$23866zqx!*ef316gni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.145.126.137', '127.0.0.1']
+ALLOWED_HOSTS = ['18.117.180.187', 'localhost']
 
 
 # Application definition
@@ -122,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
     BASE_DIR / "solpedcaf/static",  # Asegúrate de que esta carpeta exista
 ]
 
