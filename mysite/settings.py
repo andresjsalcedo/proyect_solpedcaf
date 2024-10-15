ff76af2d45493936744e9527754c55dbd5953ffb
Andrés Salcedo <134748294+andresjsalcedo@users.noreply.github.com>
2024-10-14 21:59:01 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0jb3v_ishkl=t)noy55jg^fy9m*e**w$23866zqx!*ef316gni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.145.126.137', 'localhost']
+ALLOWED_HOSTS = ['3.145.126.137', '127.0.0.1']
 
 
 # Application definition
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proyect_solpedcaf',
         'USER': 'root',
-        'PASSWORD': "root",
+        'PASSWORD': "rootsena",
         'HOST': 'localhost',
         'PORT': '3306',
     }
